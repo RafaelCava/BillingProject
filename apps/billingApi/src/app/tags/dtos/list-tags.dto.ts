632d0f5a238c360391adcab7513950acc6a85cf7
Rafael Cavalcante <rafael.software-developer@outlook.com>
2026-03-22 16:44:21 -0300

    feat: update list tags dto

--- apps/billingApi/src/app/tags/dtos/list-tags.dto.ts
+++ apps/billingApi/src/app/tags/dtos/list-tags.dto.ts
@@ -4,14 +4,12 @@ import { IsInt, IsOptional, IsString, Max, MaxLength, Min } from 'class-validato
 
 export class ListTagsDto {
   @ApiPropertyOptional({ example: 1, description: 'Numero da pagina para paginacao (padrao: 1)' })
-  @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1)
   page?: number;
 
   @ApiPropertyOptional({ example: 20, description: 'Numero de itens por pagina (padrao: 20, maximo: 100)' })
-  @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1)
